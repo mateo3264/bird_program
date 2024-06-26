9391d3f4be5702e16a44d06528a6d9535ac62f2c
Mateogit32 <36247420+mateo3264@users.noreply.github.com>
2024-06-26 16:29:09 -0500

    added abstract class and modified code

--- get_images.py
+++ get_images.py
@@ -8,23 +8,31 @@ import os
 from abc import ABC, abstractmethod
 
 class BirdDownloader(ABC):
-    def __init__(self, path=None):
+    def __init__(self, path):
         self.path = path
 
         if not os.path.isdir(self.path):
+            print('MAKING A FOLDER')
             os.mkdir(self.path)
         self.bird_data = {}
+    
+    @abstractmethod
+    def get_data(self, bird):
+        pass
+
+    def save_images(self, format):
+        for bird_name in self.bird_data:
+            for i, data_content in enumerate(self.bird_data[bird_name]):
+                filename = self.path + '\\' + bird_name + str(i) + '.' + format
+                with open(filename, 'wb') as f:
+                    print(filename)
+                    f.write(data_content)
 
 
-class BirdImageDownloader:
-    def __init__(self, path=None):
-        self.path = path
 
-        if not os.path.isdir(self.path):
-            os.mkdir(self.path)
-        self.bird_images = {}
+class BirdImageDownloader(BirdDownloader):
         
-    def get_images_of_bird(self, bird, show=False):
+    def get_data(self, bird, show=False):
         
         bird = bird.lower().replace(' ', '-')
         print(bird)
@@ -48,9 +56,9 @@ class BirdImageDownloader:
     ##            print(img_src)
                 if 'https' in img_src:
                     img_content = requests.get(img_src).content
-                    if bird not in self.bird_images:
-                        self.bird_images[bird] = []
-                    self.bird_images[bird].append(img_content)
+                    if bird not in self.bird_data:
+                        self.bird_data[bird] = []
+                    self.bird_data[bird].append(img_content)
                     image_counter += 1
                     print(f'# of images {image_counter}')
                     if show:
@@ -63,16 +71,6 @@ class BirdImageDownloader:
             
         
 
-    def save_images(self):
-        
-        
-        for bird_name in self.bird_images:
-            
-            for i, img_content in enumerate(self.bird_images[bird_name]):
-                filename = self.path + '\\' + bird_name + str(i) + '.jpg'
-                with open(filename, 'wb') as f:
-                    print(filename)
-                    f.write(img_content)
                 
             
 
@@ -82,16 +80,16 @@ if __name__ == '__main__':
     df = pd.read_excel('C:\\Users\\chave\\Downloads\\aves-de-cota-observadas.xlsx', header=1)
     df = df.head(10)
     
-    bid = BirdImageDownloader('C:\\Users\\chave\\Estudio\\programación\\birds_images')
+    bid = BirdImageDownloader('./bird_images')
     for r in df.iterrows():
         bird = r[1]['Especie']
         print('bird: ', bird)
         bid.get_images_of_bird(bird, show=False)
         
-        print(bid.bird_images.keys())
+        print(bid.bird_data.keys())
         
     print('END OF GETTING IMAGES')
-    bid.save_images()
+    bid.save_images(format='jpg')
     
     
 
