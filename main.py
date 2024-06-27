5442e1c7a95fc7228ed503349dd5204312da5824
Mateogit32 <36247420+mateo3264@users.noreply.github.com>
2024-06-27 18:14:16 -0500

    added simple stimulus difumination teaching process

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import random
 import os
+from difuminators import difuminate
 
 #ToDo: get dataset with birds around the world separated by locations
 df = pd.read_excel('./aves-de-cota-observadas.xlsx', header=1)
@@ -15,8 +16,9 @@ species = df['Especie'].tolist()
 for i in range(5):
     original_specie = random.choice(species)
     specie = original_specie.lower().strip().replace(' ', '-')
-
+    specie_difumination = difuminate(specie.replace('-', ' '), 2)
     print(original_specie)
+    print(specie_difumination)
 
 
     bird_images = './bird_images'
@@ -35,12 +37,21 @@ for i in range(5):
 
 
 
-    img = Image.open(os.path.join(bird_images, specie + str(0) + '.jpg'))
+    
 
-    plt.imshow(img)
-    plt.show()
+    
+    correct_responses = 0
+    
+    for i, specie_difu in enumerate(specie_difumination):
+        img = Image.open(os.path.join(bird_images, specie + str(correct_responses) + '.jpg'))
+        plt.imshow(img)
+        plt.show()
+        
+        print(specie_difumination[correct_responses],  ': ')
+        usu = input('Write: ')
 
-    usu = input('Name: ')
-
-    if usu.lower() == original_specie.lower():
-        print('GOOD')
+        if usu.lower() == original_specie.lower():
+            print('GOOD')
+            correct_responses += 1
+        else:
+            print(f'The correct response was: {original_specie.lower()}')
