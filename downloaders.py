127252094f3f1fa4f707ea6c6d040c217d1cea54
Mateogit32 <36247420+mateo3264@users.noreply.github.com>
2024-06-27 16:39:41 -0500

    added limit of data downloaded

--- downloaders.py
+++ downloaders.py
@@ -26,11 +26,12 @@ class BirdDownloader(ABC):
                 filename = self.path + '\\' + bird_name + str(i) + '.' + format
                 with open(filename, 'wb') as f:
                     print(filename)
+                    
                     f.write(data_content)
 
 
 class BirdAudioDownloader(BirdDownloader):
-    def get_data(self, bird):
+    def get_data(self, bird, limit=5):
         bird_name = bird.lower().replace(' ', '%20')
         url = 'https://xeno-canto.org/explore?query=' + bird_name
         
@@ -46,10 +47,21 @@ class BirdAudioDownloader(BirdDownloader):
             
             audio_srcs.append(audio_src)
 
+        audio_counter = 0
         for i, audio_src in enumerate(audio_srcs):
             audio_content = requests.get('https:' + audio_src).content
+            
             bird = bird.lower().replace(' ', '-')
-            self.bird_data[bird] = audio_content
+            
+            if bird not in self.bird_data:
+                self.bird_data[bird] = []
+
+            self.bird_data[bird].append(audio_content)
+            
+            audio_counter += 1
+            if limit is not None:
+                if audio_counter >= limit:
+                    break
 
     
             # with open('./{}{}.mp3'.format(bird_name.replace('%20', '_'), i), 'wb') as f:
@@ -57,7 +69,7 @@ class BirdAudioDownloader(BirdDownloader):
 
 class BirdImageDownloader(BirdDownloader):
         
-    def get_data(self, bird, show=False):
+    def get_data(self, bird, show=False, limit=None):
         
         bird = bird.lower().replace(' ', '-')
         print(bird)
@@ -85,6 +97,9 @@ class BirdImageDownloader(BirdDownloader):
                         self.bird_data[bird] = []
                     self.bird_data[bird].append(img_content)
                     image_counter += 1
+                    if limit is not None:
+                        if image_counter >= limit:
+                            break
                     print(f'# of images {image_counter}')
                     if show:
                         
@@ -103,19 +118,18 @@ class BirdImageDownloader(BirdDownloader):
 
 if __name__ == '__main__':
     df = pd.read_excel('./aves-de-cota-observadas.xlsx', header=1)
-    df = df.head(10)
+    df = df.head(1)
     
     bid = BirdImageDownloader('./bird_images')
     bad = BirdAudioDownloader('./bird_audios')
     for r in df.iterrows():
         bird = r[1]['Especie']
         print('bird: ', bird)
-        bid.get_data(bird)
-        bad.get_data(bird)
+        bid.get_data(bird, limit=3)
+        bad.get_data(bird, limit=3)
         
-        print(bid.bird_data.keys())
-        print(bad.bird_data.keys())
-        
+    print('# of elements in audio bird data: ', len(bad.bird_data))
+    print([type(v) for v in bad.bird_data.values()])
     print('END OF GETTING IMAGES')
     bid.save_data(format='jpg')
     bad.save_data(format='mp3')
